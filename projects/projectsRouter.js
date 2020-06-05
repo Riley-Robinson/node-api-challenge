09163de8ea43514e53875d3a9aa4802c0f6c5be7
Riley-Robinson <rrtoinfinity@gmail.com>
2020-06-05 12:38:23 -0400

    we have a delete by id,post , and get by id now

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -3,9 +3,10 @@ const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-//first the get request
+//get all of the projects
 
-router.get("/", (req, res) => {
+
+router.get("/", (req, res) => {//working
     Projects.get()
         .then((post) => {
             res.status(200).json(post);
@@ -14,4 +15,40 @@ router.get("/", (req, res) => {
             res.status(500).json({ message: "500 sever messed up" });
         });
 });
+
+//posting of the project
+router.post("/", (req, res) => {
+    const data = req.body;
+    if (!data.name || !data.description) {
+        res.status(400).json({ message: "400 im sorry lad but your missing the name and description" });
+    } else {
+        Projects.insert(data)
+            .then((post) => {
+                res.status(200).json(post);
+            })
+            .catch((err) => {
+                res.status(500).json({ message: "500 sever had a error" });
+            });
+    }
+});
+
+//get a project by id request
+router.get("/:id", (req, res) => {
+    Projects.get(req.params.id)
+        .then((project) => {
+            if (project) {
+                res.status(200).json(project);
+            } else {
+                res.status(400).json({ message: "400" });
+            }
+        });
+});
+
+//delete a project by id
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Projects.remove(id)
+        .then(() => res.status(200).json({ message: "Project has been deleted" }));
+});
+
 module.exports = router;
