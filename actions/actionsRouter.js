f2489e84264db95c0ec1d6ae69097591395f2db7
Riley-Robinson <rrtoinfinity@gmail.com>
2020-06-05 12:55:01 -0400

    we have get all actions now

--- actions/actionsRouter.js
+++ actions/actionsRouter.js
@@ -3,4 +3,11 @@ const Actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
+//We SHall Get all of the actions here
+router.get("/", (req, res) => {
+    Actions.get().then((actions) => {
+        res.status(200).json(actions);
+    });
+});
+
 module.exports = router;
